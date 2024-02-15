b351cf616ee7314cda0d26524bba0584d58cf227
Denis Levin <denis.levin15@gmail.com>
2024-02-16 01:28:28 +0900

    Implement delete and use useCallback hook

--- src/components/ExerciseListForOneUser.jsx
+++ src/components/ExerciseListForOneUser.jsx
@@ -1,24 +1,36 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import axios from "axios";
 
 const ExerciseListForOneUser = () => {
+
   const [exerciseLists, setExerciseLists] = useState([]);
-  useEffect(() => {
-    const fetchExerciseList = async () => {
+  const fetchExerciseList = useCallback(async () => {
       try {
         const response = await axios.get('http://localhost:3001/getexerciselist');
         setExerciseLists(response.data);
       } catch (err) {
         console.error(err.name + ":" + err.message)
       }
-    }
-    fetchExerciseList();
   }, [])
+  
+  useEffect(() => {
+    fetchExerciseList();
+  }, [fetchExerciseList])
+
+  const onClickDeleteExercise = useCallback(async (exerciseId) => {
+    const response = await axios.delete(`http://localhost:3001/deleteexercise/${exerciseId}`);
+    alert(response.data.message);
+    fetchExerciseList()
+  }, [fetchExerciseList])
+
+  const onClickEdit = () => {
+
+  }
   return (
     <div>
-      <table className="table">
+      <table className="table table-bordered">
         <thead className="thead-dark">
-          <tr>
+          <tr className="table-active">
             <th scope="col">No</th>
             <th scope="col">Exercise Name</th>
             <th scope="col">Exercise Duration</th>
@@ -34,9 +46,9 @@ const ExerciseListForOneUser = () => {
                 <td>{ exerciseList.exerciseName}</td>
                 <td>{ exerciseList.exerciseDuration}</td>
                 <td>{exerciseList.exerciseDate}</td>
-                <td>
-                  <button>delete</button>
-                  <button>edit</button>
+                <td className="d-flex gap-3">
+                  <button className="btn btn-primary w-100 rounded-0" onClick={onClickEdit}>edit</button>
+                  <button className="btn btn-danger w-100 rounded-0" onClick={() => onClickDeleteExercise(exerciseList._id)}>delete</button>
                 </td>
               </tr>
             )
